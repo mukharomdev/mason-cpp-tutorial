944bae98a5dbadae23a5b6d63789e1c7a2292e70
mukharomdev <mukharomdev@gmail.com>
2023-02-25 08:04:58 +0700

    modified file

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ learn cpp using mason build system
 * **docs** : Contains the doxygen config file used to generate the documentation.
 * **third_party** : Contains the third_party sources and libs. These are ignored for formatting + linting + documentation purposes.
 
+
+resources ;
+
+1. asm : http://www.ibiblio.org/gferg/ldp/GCC-Inline-Assembly-HOWTO.html#ss5.1
--- src/main.cc
+++ src/main.cc
@@ -11,6 +11,16 @@ int main()
 	int x[MAX] = {size}; // uses the constant and the read-only variable
     state = 7;           // modifies state in flib.c
     f();                // calls f() in tambah.cc
-    cout << " " << endl;
+    cout << " " << x[0]<< endl;
+    
+    /*
+    include asm into main function
+     */
+    int foo = 10, bar = 15;
+        __asm__ __volatile__("addl  %%ebx,%%eax"
+                             :"=a"(foo)
+                             :"a"(foo), "b"(bar)
+                             );
+     cout << foo << endl;
 	return 0;
 }
